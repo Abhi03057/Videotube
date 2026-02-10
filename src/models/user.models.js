3d25265e3f867509929a0749b9208957fa263692
Abhi03057 <162039025+Abhi03057@users.noreply.github.com>
2026-02-11 01:48:52 +0530

    backend updates, middleware and cloudinary setup

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -51,13 +51,7 @@ const userSchema = new Schema(
     {timestamps:true}
 )
 
-userSchema.pre("save",async function (next){
-    if(!this.modified("password")) return next()
 
-    this.password = bcrypt.hash(this.password,10)
-
-    next()
-})
 
 userSchema.methods.isPasswordCorrect = async function(password){
     return await bcrypt.compare(password, this.password)
